8e9195de79dc62f8f8ac6bb14e496cf5cf09e21b
Mikel Ward <mikel@mikelward.com>
2006-11-02 05:42:43 +0000

    Test the existence of every external object before trying to use it, just in case something changes on us.
    Declare every variable before using it so we don't emit any
    errors in strict mode.
    
    Rename showIcons() to useIcons() so we don't clash with
    Tab Mix Plus (which can cause Tabs Menu to break).  Need to
    investigate what global symbols other extensions use and
    whether I can put all my stuff inside a namespace as it looks
    like lots of extensions use lots of global symbols.

--- chrome/chromeFiles/content/tabsmenu.js
+++ chrome/chromeFiles/content/tabsmenu.js
@@ -8,16 +8,22 @@ function selectTab(event)
 {
   //window.alert("Selecting item");
   var index = event.target.getAttribute("value");
-  var container = gBrowser.mTabContainer;
-  if (container)
+  if (gBrowser)
   {
-    container.selectedIndex = index;
+    var container = gBrowser.mTabContainer;
+    if (container)
+    {
+      container.selectedIndex = index;
+    }
+    else
+    {
+      logMessage("Cannot get tab container");
+    }
   }
   else
   {
-    logMessage("Cannot get tab container");
+    logMessage("Cannot get global browser");
   }
-  gBrowser.updateCurrentBrowser();
 }
 
 // Create the tab menuitems for the current tabs and add them to the Tabs menu
@@ -27,93 +33,132 @@ function createTabsMenu()
   destroyTabsMenu();
   //window.alert("Creating menu");
   var menu = document.getElementById("menu_TabsPopup");
-  if (!menu)
+  if (menu)
   {
-    return false;
-  }
-
-  if (showActions())
-  {
-    // Add the separator to the empty menu
-    var actionSeparator = document.createElement("menuseparator");
-    if (actionSeparator)
+    if (showActions())
     {
-      menu.appendChild(actionSeparator);
-      var newItem = document.createElement("menuitem");
-      if (newItem)
+      // Add the separator to the empty menu
+      var actionSeparator = document.createElement("menuseparator");
+      if (actionSeparator)
       {
-        newItem.setAttribute("label", "New Tab");
-        newItem.setAttribute("key", "key_newNavigatorTab");
-        newItem.setAttribute("command", "cmd_newNavigatorTab");
-        newItem.setAttribute("accesskey", "n");
-      }
-      menu.insertBefore(newItem, actionSeparator);
-      var closeItem = document.createElement("menuitem");
-      if (closeItem)
-      {
-        closeItem.setAttribute("label", "Close Tab");
-        closeItem.setAttribute("key", "key_close");
-        closeItem.setAttribute("command", "cmd_close");
-        closeItem.setAttribute("accesskey", "c");
-      }
-      menu.insertBefore(closeItem, actionSeparator);
-    }
-  }
-
-  var selected = gBrowser.mPanelContainer.selectedIndex;
-  var l = gBrowser.mPanelContainer.childNodes.length;
-  for (var i = 0; i < l; i++)
-  {
-    var browser = gBrowser.getBrowserAtIndex(i);
-    var tabNumber = i + 1;
-    var title = (browser.contentTitle) ? browser.contentTitle : "(Untitled)";
-
-    var menuItem = document.createElement("menuitem");
-    if (menuItem)
-    {
-      menuItem.setAttribute("id", "tabs-menu-tab" + i);
-      menuItem.setAttribute("value", i);
-
-      if (showIcons())
-      {
-        menuItem.setAttribute("class", "menuitem-iconic");
-        menuItem.setAttribute("image", browser.mIconURL);
-        menuItem.setAttribute("current", selected == i);
-      }
-      else
-      {
-        menuItem.setAttribute("class", "menuitem-radio");
-        menuItem.setAttribute("type", "radio");
-        menuItem.setAttribute("checked", selected == i);
-      }
-
-      if (showShortcuts())
-      {
-        menuItem.setAttribute("label", tabNumber + " - " + title);
-
-        // Only the first ten items in the list have keyboard shortcuts
-        var accessKey = tabNumber % 10;
-        if (tabNumber <= 10)
+        menu.appendChild(actionSeparator);
+        var newItem = document.createElement("menuitem");
+        if (newItem)
         {
-          menuItem.setAttribute("accesskey", accessKey);
-          //menuItem.setAttribute("acceltext", "Alt+" + accessKey);
+          newItem.setAttribute("label", "New Tab");
+          newItem.setAttribute("key", "key_newNavigatorTab");
+          newItem.setAttribute("command", "cmd_newNavigatorTab");
+          newItem.setAttribute("accesskey", "n");
+          menu.insertBefore(newItem, actionSeparator);
+        }
+        else
+        {
+          logMessage("Cannot create New Tab item");
+        }
+        var closeItem = document.createElement("menuitem");
+        if (closeItem)
+        {
+          closeItem.setAttribute("label", "Close Tab");
+          closeItem.setAttribute("key", "key_close");
+          closeItem.setAttribute("command", "cmd_close");
+          closeItem.setAttribute("accesskey", "c");
+          menu.insertBefore(closeItem, actionSeparator);
+        }
+        else
+        {
+          logMessage("Cannot create Close Tab item");
         }
       }
       else
       {
-        menuItem.setAttribute("label", title);
+        logMessage("Cannot get menu separator");
       }
-
-      // XXX Why don't any of these work on Mac!?
-      // XXX This is currently being handled via the "command"
-      //     attribute of the menupopup that contains the menu
-      //menuItem.addEventListener("command", selectTab, false);
-      //menuItem.setAttribute("oncommand", "selectTab();");
-      //menuItem.setAttribute("command", "selectTab();");
-      //menuItem.addEventListener("click", selectTab, false);
-
-      menu.appendChild(menuItem);
     }
+
+    if (gBrowser)
+    {
+      if (gBrowser.mPanelContainer)
+      {
+        var selected = gBrowser.mPanelContainer.selectedIndex;
+        var l = gBrowser.mPanelContainer.childNodes.length;
+        for (var i = 0; i < l; i++)
+        {
+          var browser = gBrowser.getBrowserAtIndex(i);
+          if (browser)
+          {
+            var tabNumber = i + 1;
+            var title = (browser.contentTitle) ? browser.contentTitle : "(Untitled)";
+
+            var menuItem = document.createElement("menuitem");
+            if (menuItem)
+            {
+              menuItem.setAttribute("id", "tabs-menu-tab" + i);
+              menuItem.setAttribute("value", i);
+
+              if (useIcons())
+              {
+                menuItem.setAttribute("class", "menuitem-iconic");
+                menuItem.setAttribute("image", browser.mIconURL);
+                menuItem.setAttribute("current", selected == i);
+              }
+              else
+              {
+                menuItem.setAttribute("class", "menuitem-radio");
+                menuItem.setAttribute("type", "radio");
+                menuItem.setAttribute("checked", selected == i);
+              }
+
+              if (showShortcuts())
+              {
+                menuItem.setAttribute("label", tabNumber + " - " + title);
+
+                // Only the first ten items in the list have keyboard shortcuts
+                var accessKey = tabNumber % 10;
+                if (tabNumber <= 10)
+                {
+                  menuItem.setAttribute("accesskey", accessKey);
+                  //menuItem.setAttribute("acceltext", "Alt+" + accessKey);
+                }
+              }
+              else
+              {
+                menuItem.setAttribute("label", title);
+              }
+
+              // XXX Why don't any of these work on Mac!?
+              // XXX This is currently being handled via the "command"
+              //     attribute of the menupopup that contains the menu
+              //menuItem.addEventListener("command", selectTab, false);
+              //menuItem.setAttribute("oncommand", "selectTab();");
+              //menuItem.setAttribute("command", "selectTab();");
+              //menuItem.addEventListener("click", selectTab, false);
+
+              menu.appendChild(menuItem);
+            }
+            else
+            {
+              logMessage("Cannot create menu item");
+            }
+          }
+          else
+          {
+            logMessage("Cannot get current browser");
+          }
+        }
+      }
+      else
+      {
+        logMessage("Cannot get panel container");
+      }
+    }
+    else
+    {
+      logMessage("Cannot get global browser");
+    }
+  }
+  else
+  {
+    logMessage("Cannot get menu root");
   }
 }
 
@@ -122,14 +167,13 @@ function destroyTabsMenu()
 {
   //window.alert("Destroying menu");
   var menu = document.getElementById("menu_TabsPopup");
-  var length = menu.childNodes.length;
-  for (var i = length - 1; i >= 0; i--)
+  if (menu)
   {
-    var tabNumber = i + 1;
-    var node = menu.childNodes[i];
-    // XXX or maybe node.getAttribute("class") == "menuitem-radio"
-    //if (node.getAttribute("value"))
-    //{
+    var length = menu.childNodes.length;
+    for (var i = length - 1; i >= 0; i--)
+    {
+      var tabNumber = i + 1;
+      var node = menu.childNodes[i];
       try
       {
         menu.removeChild(node);
@@ -138,20 +182,38 @@ function destroyTabsMenu()
       {
         logMessage("Failed to remove menu item " + tabNumber + ": " + label);
       }
-    //}
+    }
+  }
+  else
+  {
+    logMessage("Cannot get menu root");
   }
 }
 
 // Print the names of all the tabs in the current window
 function dumpTabs()
 {
-  var l = gBrowser.mPanelContainer.childNodes.length;
-  for (var i = 0; i < l; i++)
+  if (gBrowser)
   {
-    var browser = gBrowser.getBrowserAtIndex(i);
-    var tabNumber = i + 1;
-    var title = (browser.contentTitle) ? browser.contentTitle : "(Untitled)";
-    logMessage(tabNumber + ": " + title);
+    if (gBrowser.mPanelContainer)
+    {
+      var l = gBrowser.mPanelContainer.childNodes.length;
+      for (var i = 0; i < l; i++)
+      {
+        var browser = gBrowser.getBrowserAtIndex(i);
+        var tabNumber = i + 1;
+        var title = (browser.contentTitle) ? browser.contentTitle : "(Untitled)";
+        logMessage(tabNumber + ": " + title);
+      }
+    }
+    else
+    {
+      logMessage("Cannot get panel container");
+    }
+  }
+  else
+  {
+    logMessage("Cannot get global browser");
   }
 }
 
@@ -160,8 +222,8 @@ function logMessage(message)
 {
   try
   {
-    log = Components.classes["@mozilla.org/consoleservice;1"]
-                    .getService(Components.interfaces.nsIConsoleService);
+    var log = Components.classes["@mozilla.org/consoleservice;1"]
+                        .getService(Components.interfaces.nsIConsoleService);
     log.logStringMessage(message);
   }
   catch(e)
@@ -176,9 +238,9 @@ function showActions()
 {
   try
   {
-    prefs = Components.classes["@mozilla.org/preferences-service;1"]
-                      .getService(Components.interfaces.nsIPrefService)
-                      .getBranch("extensions.tabsmenu.");
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                          .getService(Components.interfaces.nsIPrefService)
+                          .getBranch("extensions.tabsmenu.");
     return prefs.getBoolPref("showactions");
   } 
   catch(e)
@@ -190,13 +252,13 @@ function showActions()
 
 // Check whether the user wants to see each page's icon next to
 // the title in the menu.  If not, we show a radio button.
-function showIcons()
+function useIcons()
 {
   try
   {
-    prefs = Components.classes["@mozilla.org/preferences-service;1"]
-                      .getService(Components.interfaces.nsIPrefService)
-                      .getBranch("extensions.tabsmenu.");
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                          .getService(Components.interfaces.nsIPrefService)
+                          .getBranch("extensions.tabsmenu.");
     return prefs.getBoolPref("showicons");
   } 
   catch(e)
@@ -213,9 +275,9 @@ function showShortcuts()
 {
   try
   {
-    prefs = Components.classes["@mozilla.org/preferences-service;1"]
-                      .getService(Components.interfaces.nsIPrefService)
-                      .getBranch("extensions.tabsmenu.");
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                          .getService(Components.interfaces.nsIPrefService)
+                          .getBranch("extensions.tabsmenu.");
     return prefs.getBoolPref("showshortcuts");
   } 
   catch(e)
