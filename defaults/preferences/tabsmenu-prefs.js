5cc5fcb306f3ef41ce98a595c9def5b3a7a5b12e
Mikel Ward <mikel@mikelward.com>
2006-11-08 14:55:23 +0000

    Internationalise the options dialog.

--- defaults/preferences/tabsmenu-prefs.js
+++ defaults/preferences/tabsmenu-prefs.js
@@ -1,3 +1,4 @@
 pref("extensions.tabsmenu.showactions", false);
 pref("extensions.tabsmenu.showicons", true);
 pref("extensions.tabsmenu.showshortcuts", false);
+pref("extensions.{ec8030f7-c20a-464f-9b0e-13a3a9e97384}.description", "chrome://tabsmenu/locale/tabsmenu.properties");
