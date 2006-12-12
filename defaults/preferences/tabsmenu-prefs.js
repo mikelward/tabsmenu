4536e5fe9fdf7918ebde1891dbd04d64056a0807
Mikel Ward <mikel@mikelward.com>
2006-12-12 00:20:38 +0000

    Fix up the platform preferences.  The subdirectory should have been called defaults rather than default.  Also add a default sane global setting for the default tab icon for other platforms (e.g. UNIX).

--- defaults/preferences/tabsmenu-prefs.js
+++ defaults/preferences/tabsmenu-prefs.js
@@ -2,3 +2,4 @@ pref("extensions.tabsmenu.showactions", false);
 pref("extensions.tabsmenu.showicons", true);
 pref("extensions.tabsmenu.showshortcuts", false);
 pref("extensions.{dc5d9a10-2736-11da-8cd6-0800200c9a66}.description", "chrome://tabsmenu/locale/tabsmenu.properties");
+pref("extensions.tabsmenu.defaulttabicon", "chrome://browser/skin/Bookmarks-folder.png");
